36324f7c0d7744c7e7bc1d28cab56d5cdb7ada75
unknown <akhigbesadiq@gmail.com>
2025-08-20 09:09:56 +0100

    Browser router added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { store } from './app/store';
 
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
